aadd7092d089c1638a6bc2a89955f469dd7ddd3c
akarat <akrs33@gmail.com>
2019-04-06 01:11:31 +0700

    add try catch

--- routes/line/bot.line.controller.js
+++ routes/line/bot.line.controller.js
@@ -93,41 +93,43 @@ function buildReceipt(order) {
 
 function webhook(req,res){
     //console.log(req.body.events[0])
-    if(req.body.events[0].type == 'join'){
-        //send reply message to let member setting siteId
-        //line_replyMessage(req.body.events[0].replyToken ,{ type: 'flex',altText:'Group Join', contents: joinmessage });
+    try {
+        if (req.body.events[0].type == 'join') {
+            //send reply message to let member setting siteId
+            //line_replyMessage(req.body.events[0].replyToken ,{ type: 'flex',altText:'Group Join', contents: joinmessage });
 
-    }else if(req.body.events[0].type == 'leave'){
+        } else if (req.body.events[0].type == 'leave') {
 
-    }else if(req.body.events[0].type == 'message'){
-        if( req.body.events[0].message.type == 'text') {
-            replyToken = req.body.events[0].replyToken
-            msgText = req.body.events[0].message.text
+        } else if (req.body.events[0].type == 'message') {
+            if (req.body.events[0].message.type == 'text') {
+                replyToken = req.body.events[0].replyToken
+                msgText = req.body.events[0].message.text
 
-            if(checkPrefix('^(@tfex)')) {
-                msgText.replace('     ', ' ')
-                msgText.replace('    ', ' ')
-                msgText.replace('   ', ' ')
-                msgText.replace('  ', ' ')
+                if (checkPrefix('^(@tfex)')) {
+                    msgText.replace('     ', ' ')
+                    msgText.replace('    ', ' ')
+                    msgText.replace('   ', ' ')
+                    msgText.replace('  ', ' ')
 
-                command = msgText.split(' ')
-                if (command[1] == 'day') {
-                    loadSTD('day',replyToken)
-                    res.json({status: 'ok'})
-                }else if(command[1] == 'week'){
-                    loadSTD('week',replyToken)
-                    res.json({status: 'ok'})
-                }else{
-                    loadSTD('all',replyToken)
-                    res.json({status: 'ok'})
+                    command = msgText.split(' ')
+                    if (command[1] == 'day') {
+                        loadSTD('day', replyToken)
+                        res.json({status: 'ok'})
+                    } else if (command[1] == 'week') {
+                        loadSTD('week', replyToken)
+                        res.json({status: 'ok'})
+                    } else {
+                        loadSTD('all', replyToken)
+                        res.json({status: 'ok'})
+                    }
+                } else {
+                    line_replyMessage(replyToken, {type: 'text', text: msgText});
                 }
-            }else{
-                line_replyMessage(replyToken ,{ type: 'text',text:msgText});
             }
         }
-    }
-    //next(req,res)
-    res.json(req.body.events) // req.body will be webhook event object
+        //next(req,res)
+        res.json(req.body.events) // req.body will be webhook event object
+    }catch(e){res.send(e)}
 }
 
 module.exports = { webhook ,middleware ,handlePreErr};
